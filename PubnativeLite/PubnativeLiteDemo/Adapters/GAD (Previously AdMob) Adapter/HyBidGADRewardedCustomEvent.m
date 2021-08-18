8d0f88395edc8b824efb97118059f952919666a2
Fares Benhamouda <fares.bnhamouda@gmail.com>
2021-08-18 13:37:20 +0200

    update beta3 to reflect typo fix on excluded archs.

--- PubnativeLite/PubnativeLiteDemo/Adapters/GAD (Previously AdMob) Adapter/HyBidGADRewardedCustomEvent.m
+++ PubnativeLite/PubnativeLiteDemo/Adapters/GAD (Previously AdMob) Adapter/HyBidGADRewardedCustomEvent.m
@@ -107,7 +107,7 @@ typedef id<GADMediationRewardedAdEventDelegate> _Nullable(^HyBidGADRewardedCusto
 
 #pragma mark - GADMediationAdapter
 
-// v: 2.5.4-beta2
+// v: 2.5.4-beta3
 + (GADVersionNumber)adSDKVersion {
     GADVersionNumber version = {0};
     version.majorVersion = 2;
--- PubnativeLite/PubnativeLiteDemo/Adapters/MoPubAdapter/HyBidAdapterConfiguration.m
+++ PubnativeLite/PubnativeLiteDemo/Adapters/MoPubAdapter/HyBidAdapterConfiguration.m
@@ -23,8 +23,8 @@
 #import "HyBidAdapterConfiguration.h"
 
 NSString *const HyBidAdapterConfigurationNetworkName = @"pubnative";
-NSString *const HyBidAdapterConfigurationAdapterVersion = @"2.5.4-beta2.0";
-NSString *const HyBidAdapterConfigurationNetworkSDKVersion = @"2.5.4-beta2";
+NSString *const HyBidAdapterConfigurationAdapterVersion = @"2.5.4-beta3.0";
+NSString *const HyBidAdapterConfigurationNetworkSDKVersion = @"2.5.4-beta3";
 NSString *const HyBidAdapterConfigurationAppTokenKey = @"pubnative_appToken";
 
 @implementation HyBidAdapterConfiguration
