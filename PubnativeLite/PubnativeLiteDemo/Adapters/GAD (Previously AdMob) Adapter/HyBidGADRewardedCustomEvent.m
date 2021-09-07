a7fe33d6d3115f8090ef96ce12869b5d3509cef5
Fares Benhamouda <fares.bnhamouda@gmail.com>
2021-09-07 16:30:42 +0200

    prep 2.6.2-beta3

--- PubnativeLite/PubnativeLiteDemo/Adapters/GAD (Previously AdMob) Adapter/HyBidGADRewardedCustomEvent.m
+++ PubnativeLite/PubnativeLiteDemo/Adapters/GAD (Previously AdMob) Adapter/HyBidGADRewardedCustomEvent.m
@@ -107,7 +107,7 @@ typedef id<GADMediationRewardedAdEventDelegate> _Nullable(^HyBidGADRewardedCusto
 
 #pragma mark - GADMediationAdapter
 
-// v: 2.6.2-beta2
+// v: 2.6.2-beta3
 + (GADVersionNumber)adSDKVersion {
     GADVersionNumber version = {0};
     version.majorVersion = 2;
